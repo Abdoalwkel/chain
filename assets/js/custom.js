70ef8098785ca62ea24c8b88f628a0a39f2dfd18
abdoAlwkel <alwkel138@gmail.com>
2023-09-19 22:17:42 +0300

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,47 @@
+(function ($) {
+	
+
+
+	$('.loop').owlCarousel({
+      center: true,
+      items:1,
+      loop:true,
+      autoplay: true,
+      nav: true,
+      margin:0,
+      responsive:{ 
+          1200:{
+              items:5
+          },
+          992:{
+              items:3
+          },
+          760:{
+            items:2
+        }
+      }
+  });
+  
+
+
+  if($('.menu-trigger').length){
+    $(".menu-trigger").on('click', function() { 
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
+    });
+  }
+
+  function mobileNav() {
+    var width = $(window).width();
+    $('.submenu').on('click', function() {
+      if(width < 767) {
+        $('.submenu ul').removeClass('active');
+        $(this).find('ul').toggleClass('active');
+      }
+    });
+  }
+
+
+
+
+})(window.jQuery);
